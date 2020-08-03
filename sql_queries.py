df80827cb1f26a828c68542deef02c02466105fb
Kevin Anthony Dsouza <kevindsouza1188@gmail.com>
2020-08-03 01:03:32 -0400

    Updates users table to update level on conflict

--- sql_queries.py
+++ sql_queries.py
@@ -27,7 +27,7 @@ songplay_table_insert = ("""INSERT INTO songplays (start_time, user_id, level, s
 VALUES(%s, %s, %s, %s, %s, %s, %s, %s);""")
 
 user_table_insert = ("""INSERT INTO users (user_id, first_name, last_name, gender, level)
-VALUES(%s, %s, %s, %s, %s) ON CONFLICT(user_id) DO NOTHING;""")
+VALUES(%s, %s, %s, %s, %s) ON CONFLICT(user_id) DO UPDATE SET level=users.level;""")
 
 song_table_insert = ("""INSERT INTO songs (song_id, title, artist_id, year, duration)
 VALUES(%s, %s, %s, %s, %s) ON CONFLICT(song_id) DO NOTHING;""")
